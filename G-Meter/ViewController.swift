54ddb37e1e0a08cedec45144fdbe1327272a5a69
Albert Loh <al123@als-macbook-pro-4.local>
2017-05-11 21:31:43 +0800

    Initial Commit

---
+++ G-Meter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  G-Meter
+//
+//  Created by Albert Loh on 11/5/17.
+//  Copyright © 2017 Albert Loh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
